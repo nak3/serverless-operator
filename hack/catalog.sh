ea4107e815c8577511af31775ecba2e4992f77be
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2020-04-24 13:21:35 +0900

    tmp

--- hack/catalog.sh
+++ hack/catalog.sh
@@ -20,7 +20,7 @@ rm -rf $CRD_DIR
 mkdir $CRD_DIR
 
 # deal with identical CRD's in nested dirs: highest version wins
-find $OLM_DIR -name '*_crd.yaml' | sort -n | xargs -I{} cp {} $CRD_DIR/
+find $OLM_DIR -name '*_crd.yaml' | grep "${VERSION}" | sort -n | xargs -I{} cp {} $CRD_DIR/
 
 # Determine if we're running locally or in CI.
 if [ -n "$OPENSHIFT_BUILD_NAMESPACE" ]; then
@@ -35,7 +35,7 @@ else
 fi
 
 CRD=$(cat $(ls $CRD_DIR/*) | grep -v -- "---" | indent apiVersion)
-CSV=$(cat $(find $OLM_DIR -name '*version.yaml' | sort -n) | envsubst '$IMAGE_KNATIVE_OPERATOR $IMAGE_KNATIVE_OPENSHIFT_INGRESS' | indent apiVersion)
+CSV=$(cat $(find $OLM_DIR -name '*version.yaml' | grep "${VERSION}" | sort -n) | envsubst '$IMAGE_KNATIVE_OPERATOR $IMAGE_KNATIVE_OPENSHIFT_INGRESS' | indent apiVersion)
 PKG=$(cat $OLM_DIR/$NAME/*package.yaml | indent packageName)
 
 cat <<EOF | sed 's/^  *$//'
